a10499f13da3c2816312d72505dffca7b1a757ae
Douglass Davidoff <douglass.davidoff@gmail.com>
2019-11-15 08:48:00 -0500

    Adding first game functionality tests

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,14 +19,27 @@ function decrement() {
   }
 }
 
-run();
 
+
+$("#startButton").on("click", function() {
+  $(this).hide();
+  var firstQuestion = myQuestions[0];
+  var answerA = myQuestions[0].answers.a;
+  var quiz = $("#quiz");
+  run();
+  quiz.html("<h2>" + firstQuestion.question + "</h2><br>");
+  quiz.append("<button><h2>" + myQuestions[0].answers.a + "</h2></button><br><br>");
+  quiz.append("<button><h2>" + myQuestions[0].answers.b + "</h2></button><br><br>");
+  quiz.append("<button><h2>" + myQuestions[0].answers.c + "</h2></button><br><br>");
+  quiz.append("<button><h2>" + myQuestions[0].answers.d + "</h2></button><br><br>");
+  // quiz.append("<div>" + number + "</div>");
+})
 
 function stop() {
 clearInterval(intervalId);
 }
 
-(function() {
+// (function() {
   function buildQuiz() {
     // we'll need a place to store the HTML output
     const output = [];
@@ -98,13 +111,13 @@ clearInterval(intervalId);
     {
       question: "Chicago-O’Hare International Airport’s three-letter code is “ORD.” But there is no “D” in O’Hare’s name. What’s the origin of the “ORD” code?",
       answers: {
-        a: "The 'D' indicated Chicago’s located on Lake Michigan.",
+        a: "The 'D' indicated Chicago’s location on Lake Michigan.",
         b: "O’Hare Depot, the suburban rail freight yard which grew into O’Hare Airport.",
         c: "Orchard Park, the village now covered by O’Hare’s airfield.",
         d: "Army Col. Dwight O’Hare, the WWII flying ace for whom the airport was named."
       },
-      correctAnswer: "c"
-      // image: "https://cdn.shopify.com/s/files/1/0322/6285/products/ORD-airport-code-luggage-tag_1_2000x.jpg?v=1556169336"
+      correctAnswer: "c",
+        image: "https://cdn.shopify.com/s/files/1/0322/6285/products/ORD-airport-code-luggage-tag_1_2000x.jpg?v=1556169336"
     },
     {
       question: "The Panama Canal runs in which directions?",
@@ -114,8 +127,8 @@ clearInterval(intervalId);
         c: "Northeast-Southwest",
         d: "Northwest-Southeast"
       },
-      correctAnswer: "d"
-      // image: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-540x360/06/95/f3/17.jpg"
+      correctAnswer: "d",
+      image: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-540x360/06/95/f3/17.jpg"
     },
     {
       question: "Where is Waldo, really?",
@@ -125,8 +138,8 @@ clearInterval(intervalId);
         c: "Sitting in a tree",
         d: "Minding his own business, so stop asking"
       },
-      correctAnswer: "d"
-      // image: "https://www.saturnbooksellers.com/sites/saturnbooksellers.com/files/wheres-waldo_0_0.jpg"
+      correctAnswer: "d",
+      image: "https://www.saturnbooksellers.com/sites/saturnbooksellers.com/files/wheres-waldo_0_0.jpg"
     },
     {
       question: "Before the Berlin was united in 1989, the city was administered by the four World War II Allies: France, the Soviet Union, the United Kingdom, and the United States. “Checkpoint Charlie” allowed travelers to cross from East Berlin into the sector administered by:",
@@ -136,8 +149,8 @@ clearInterval(intervalId);
         c: "United Kingdom.",
         d: "United States."
       },
-      correctAnswer: "d"
-      // image: "https://upload.wikimedia.org/wikipedia/commons/7/7d/US_Army_tanks_face_off_against_Soviet_tanks%2C_Berlin_1961.jpg"
+      correctAnswer: "d",
+      image: "https://upload.wikimedia.org/wikipedia/commons/7/7d/US_Army_tanks_face_off_against_Soviet_tanks%2C_Berlin_1961.jpg"
     },
     {
       question: "The Statue of Liberty is in New York.",
@@ -147,8 +160,8 @@ clearInterval(intervalId);
         c: "No, it's a federal territory.",
         d: "No, it lies in international waters."
       },
-      correctAnswer: "b"
-      // image: "https://www.nps.gov/stli/planyourvisit/images/Liberty-statue-with-manhattan_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false"
+      correctAnswer: "b",
+      image: "https://www.nps.gov/stli/planyourvisit/images/Liberty-statue-with-manhattan_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false"
     },
     {
       question: "Which of these is not true?",
@@ -158,8 +171,8 @@ clearInterval(intervalId);
         c: "Rhode Island borders on New York.",
         d: "Ontario lies south of Michigan."
       },
-      correctAnswer: "a"
-      // mage: "https://i.etsystatic.com/7741241/r/il/aef6fe/1673920913/il_1588xN.1673920913_ognh.jpg"
+      correctAnswer: "a",
+      image: "https://i.etsystatic.com/7741241/r/il/aef6fe/1673920913/il_1588xN.1673920913_ognh.jpg"
     },
     {
       question: "Stamford is located at approximately 41 degrees north latitude. It shares this property with all of the below except:",
@@ -169,8 +182,8 @@ clearInterval(intervalId);
         c: "Malta, the Mediterrean island nation.",
         d: "Border of Colorado and Wyoming."
       },
-      correctAnswer: "c"
-      // image: "assets/images/41stParallelNorth.png"
+      correctAnswer: "c",
+      image: "assets/images/41stParallelNorth.png"
     },
     {
       question: "Connecticut encompasses how many cities and towns?",
@@ -180,8 +193,8 @@ clearInterval(intervalId);
         c: "169.",
         d: "196."
       },
-      correctAnswer: "c"
-      // image: "https://www.chuckstraub.com/images/conntownandcounty.jpg"
+      correctAnswer: "c",
+      image: "https://www.chuckstraub.com/images/conntownandcounty.jpg"
     },
     {
       question: "During the summer, Hartford-Bradley Airport becomes the third busiest in New England. Which airport becomes the second-busiest?",
@@ -191,14 +204,14 @@ clearInterval(intervalId);
         c: "Portland (Maine) Jetport.",
         d: "Bridgeport-Sikorsky."
       },
-      correctAnswer: "b"
-      // image: "https://chambermaster.blob.core.windows.net/images/customers/2764/members/1430/photos/GALLERY_MAIN/8B5CFE2E-4BDF-4DFB-82E9-F81165DE3F97athsd1.ma.comcast.net.jpeg"
+      correctAnswer: "b",
+      image: "https://chambermaster.blob.core.windows.net/images/customers/2764/members/1430/photos/GALLERY_MAIN/8B5CFE2E-4BDF-4DFB-82E9-F81165DE3F97athsd1.ma.comcast.net.jpeg"
     }
   ];
 
   // display quiz right away
-  buildQuiz();
+  // buildQuiz();
 
   // on submit, show results
-  submitButton.addEventListener("click", showResults);
-})();
+  // submitButton.addEventListener("click", showResults);
+// })();
